8845d22f3d217464aba56eb0e8ff561f15d65b2c
Yutong Sun <yutongsu@amazon.com>
2025-12-31 01:38:55 +0000

    kubernetes: add fail_cgroup_v1 setting
    Signed-off-by: Yutong Sun <yutongsu@amazon.com>

--- bottlerocket-settings-models/settings-extensions/kubernetes/src/lib.rs
+++ bottlerocket-settings-models/settings-extensions/kubernetes/src/lib.rs
@@ -100,6 +100,7 @@ pub struct KubernetesSettingsV1 {
     hostname_override: ValidLinuxHostname,
     ids_per_pod: KubernetesIdsPerPodValue,
     max_parallel_image_pulls: i32,
+    fail_cgroup_v1: bool,
 }
 
 type Result<T> = std::result::Result<T, Infallible>;
@@ -207,6 +208,7 @@ mod test {
                 static_pods_enabled: None,
                 ids_per_pod: None,
                 max_parallel_image_pulls: None,
+                fail_cgroup_v1: None,
             })
         );
     }
